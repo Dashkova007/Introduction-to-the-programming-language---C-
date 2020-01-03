8d3c601b49496d92681805e8b8b13bde03dfab91
Dashkova007 <51876166+Dashkova007@users.noreply.github.com>
2020-01-03 15:57:15 +0200

    commit

---
+++ Introduction to the programming language - C++/Introduction to the programming language - C++.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<iomanip>
+using namespace std;
+void main() {
+	cout << setw(1) << setfill((char)201) << "";
+	cout << setw(35) << setfill((char)205) << "";
+	cout << setw(1) << setfill((char)187) << "" << endl;
+	cout << setw(1) << setfill((char)186) << "";
+	cout << "              Seasons              ";
+	cout << setw(1) << setfill((char)186) << "" << endl;
+
+	cout << setw(1) << setfill((char)204) << "";
+	cout << setw(8) << setfill((char)205) << "";
+	cout << setw(1) << setfill((char)203) << "";
+	cout << setw(8) << setfill((char)205) << "";
+	cout << setw(1) << setfill((char)203) << "";
+	cout << setw(8) << setfill((char)205) << "";
+	cout << setw(1) << setfill((char)203) << "";
+	cout << setw(8) << setfill((char)205) << "";
+	cout << setw(1) << setfill((char)185) << "" << endl;
+
+	cout << setw(1) << setfill((char)186) << "";
+	cout << " Winter ";
+	cout << setw(1) << setfill((char)186) << "";
+	cout << " Spring ";
+	cout << setw(1) << setfill((char)186) << "";
+	cout << " Summer ";
+	cout << setw(1) << setfill((char)186) << "";
+	cout << " Autumn ";
+	cout << setw(1) << setfill((char)186) << "" << endl;
+
+	cout << setw(1) << setfill((char)200) << "";
+	cout << setw(8) << setfill((char)205) << "";
+	cout << setw(1) << setfill((char)202) << "";
+	cout << setw(8) << setfill((char)205) << "";
+	cout << setw(1) << setfill((char)202) << "";
+	cout << setw(8) << setfill((char)205) << "";
+	cout << setw(1) << setfill((char)202) << "";
+	cout << setw(8) << setfill((char)205) << "";
+	cout << setw(1) << setfill((char)188) << "" << endl;
+	system("pause");
+}
